fd66fcb7e07910c19e001890a34b6fb4182d477c
Oliver Downard <olivermd@btinternet.com>
2017-12-17 11:49:39 +0000

    Add misc extras from previous work

---
+++ include/GreaterThanGame.h
@@ -0,0 +1,24 @@
+//
+// Created by odownard on 02/11/17.
+//
+
+#ifndef GREATERTHANGAME_GREATERTHANGAME_H
+#define GREATERTHANGAME_GREATERTHANGAME_H
+
+#include <cstdint>
+
+namespace GTG {
+    class GreaterThanPlayer {
+    public:
+        uint16_t GetRepr() { return value; }
+        void mutate();
+    private:
+        uint16_t value;
+    };
+    class GreaterThanGame {
+    public:
+        uint8_t PlayTwo(GreaterThanPlayer p1, GreaterThanPlayer p2);
+    };
+}
+
+#endif //GREATERTHANGAME_GREATERTHANGAME_H

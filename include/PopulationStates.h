2a2727c284c3ce090989540cc9dc52b6923786cb
Oliver Downard <olivermd@btinternet.com>
2017-12-18 16:12:48 +0000

    Add population state skeletons

--- include/PopulationStates.h
+++ include/PopulationStates.h
@@ -3,4 +3,22 @@
 //
 #pragma once
 
+#include "include/Population.h"
 
+class GeneratePopState : public PopulationState {
+public:
+  std::unique_ptr<PopulationState> execute(Population &pop) override;
+  std::string name() const override { return "GeneratePopState"; }
+};
+
+class EvaluateFitnessState : public PopulationState {
+public:
+  std::unique_ptr<PopulationState> execute(Population &pop) override;
+  std::string name() const override { return "EvaluateFitnessState"; }
+};
+
+class VariationState : public PopulationState {
+public:
+  std::unique_ptr<PopulationState> execute(Population &pop) override;
+  std::string name() const override { return "VariationState"; }
+};
